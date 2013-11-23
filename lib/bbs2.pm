c5b6f56d244af5b29e2c48562c9d0b3931446d0e
kaoru yamabe <shikisokuzeku.822@gmail.com>
2013-11-24 02:00:28 +0900

    initial import

---
+++ lib/bbs2.pm
@@ -0,0 +1,47 @@
+package bbs2;
+use strict;
+use warnings;
+use utf8;
+our $VERSION='0.01';
+use 5.008001;
+use bbs2::DB::Schema;
+use bbs2::DB;
+
+use parent qw/Amon2/;
+# Enable project local mode.
+__PACKAGE__->make_local_context();
+
+my $schema = bbs2::DB::Schema->instance;
+
+sub db {
+    my $c = shift;
+    if (!exists $c->{db}) {
+        my $conf = $c->config->{DBI}
+            or die "Missing configuration about DBI";
+        $c->{db} = bbs2::DB->new(
+            schema       => $schema,
+            connect_info => [@$conf],
+            # I suggest to enable following lines if you are using mysql.
+            # on_connect_do => [
+            #     'SET SESSION sql_mode=STRICT_TRANS_TABLES;',
+            # ],
+        );
+    }
+    $c->{db};
+}
+
+1;
+__END__
+
+=head1 NAME
+
+bbs2 - bbs2
+
+=head1 DESCRIPTION
+
+This is a main context class for bbs2
+
+=head1 AUTHOR
+
+bbs2 authors.
+
---
+++ t/00_compile.t
@@ -0,0 +1,17 @@
+use strict;
+use warnings;
+use Test::More;
+
+
+use bbs2;
+use bbs2::Web;
+use bbs2::Web::View;
+use bbs2::Web::ViewFunctions;
+
+use bbs2::DB::Schema;
+use bbs2::Web::Dispatcher;
+
+
+pass "All modules can load.";
+
+done_testing;
